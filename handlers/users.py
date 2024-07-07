5bc3df358dd935d67c6b1c8d00f28fa44660fe62
valeriy <valeriysimachenkov@gmail.com>
2024-07-07 19:27:41 +0300

    INFO:added catalog buttons

--- handlers/users.py
+++ handlers/users.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F
 from aiogram.filters import Command
 from aiogram.types import Message, ReplyKeyboardMarkup
-from keyboards.user_kb import kb_start, kb_catalog  # Кнопки
+from keyboards.user_kb import kb_start, kb_catalog, kb_nike, kb_fila, kb_vans, kb_puma, kb_adidas  # Кнопки
 from aiogram.types import CallbackQuery
 
 
@@ -18,8 +18,33 @@ async def cmd_start(message: Message):
 
 @router.callback_query(F.data.in_(['catalog_pressed']))
 async def nike_catalog(callback: CallbackQuery):
-    await callback.message.edit_text(text="Коллекция nike", reply_markup=kb_catalog)
+    await callback.message.edit_text(text="Наша коллекция обуви", reply_markup=kb_catalog)
 
-@router.callback_query(F.data.in_(['back_pressed']))
+@router.callback_query(F.data.in_(['back_to_start']))
 async def back_press(callback: CallbackQuery):
     await callback.message.edit_text(text="И снова вы в главном меню", reply_markup=kb_start)
+
+@router.callback_query(F.data.in_(['nike_pressed']))
+async def back_press(callback: CallbackQuery):
+    await callback.message.edit_text(text="Nike", reply_markup=kb_nike)
+
+@router.callback_query(F.data.in_(['adidas_pressed']))
+async def back_press(callback: CallbackQuery):
+    await callback.message.edit_text(text="Adidas", reply_markup=kb_adidas)
+
+@router.callback_query(F.data.in_(['puma_pressed']))
+async def back_press(callback: CallbackQuery):
+    await callback.message.edit_text(text="Puma", reply_markup=kb_puma)
+
+@router.callback_query(F.data.in_(['vans_pressed']))
+async def back_press(callback: CallbackQuery):
+    await callback.message.edit_text(text="Vans", reply_markup=kb_vans)
+
+@router.callback_query(F.data.in_(['fila_pressed']))
+async def back_press(callback: CallbackQuery):
+    await callback.message.edit_text(text="Fila", reply_markup=kb_fila)
+
+
+
+
+
--- keyboards/user_kb.py
+++ keyboards/user_kb.py
@@ -14,16 +14,84 @@ kb_start = InlineKeyboardMarkup(inline_keyboard=start_btn)
 # Профиль
 btn_catalog = [
     [
-        InlineKeyboardButton(text="Nike", callback_data="Nike"),
-        InlineKeyboardButton(text="Adidas", callback_data="Adidas"),
-        InlineKeyboardButton(text="Puma", callback_data="Puma"),
-        InlineKeyboardButton(text="Vans", callback_data="Vans"),
-        InlineKeyboardButton(text="Fila", callback_data="Fila"),
+        InlineKeyboardButton(text="Nike", callback_data="nike_pressed"),
+        InlineKeyboardButton(text="Adidas", callback_data="adidas_pressed"),
+        InlineKeyboardButton(text="Puma", callback_data="puma_pressed"),
+        InlineKeyboardButton(text="Vans", callback_data="vans_pressed"),
+        InlineKeyboardButton(text="Fila", callback_data="fila_pressed"),
     ],
     
     [
-        InlineKeyboardButton(text="Назад", callback_data="back_pressed")
+        InlineKeyboardButton(text="Вернуться", callback_data="back_to_start")
     ]
 ]
-
 kb_catalog = InlineKeyboardMarkup(inline_keyboard=btn_catalog)
+
+
+
+
+btn_nike = [
+    [
+        InlineKeyboardButton(text="Вперед", callback_data="next_nike"), # кнопки которые переключают модель кед
+        InlineKeyboardButton(text="Назад", callback_data="back_nike")
+    ],
+    [
+        InlineKeyboardButton(text="Вернуться", callback_data="catalog_pressed")
+    ]
+] 
+kb_nike = InlineKeyboardMarkup(inline_keyboard=btn_nike)
+
+
+
+
+btn_adidas = [
+    [
+        InlineKeyboardButton(text="Вперед", callback_data="next_adidas"), # кнопки которые переключают модель кед
+        InlineKeyboardButton(text="Назад", callback_data="back_adidas")
+    ],
+    [
+        InlineKeyboardButton(text="Вернуться", callback_data="catalog_pressed")
+    ]
+]
+kb_adidas = InlineKeyboardMarkup(inline_keyboard=btn_adidas)
+
+
+
+
+btn_puma = [
+    [
+        InlineKeyboardButton(text="Вперед", callback_data="next_puma"), # кнопки которые переключают модель кед
+        InlineKeyboardButton(text="Назад", callback_data="back_puma")
+    ],
+    [
+        InlineKeyboardButton(text="Вернуться", callback_data="catalog_pressed")
+    ]
+] 
+kb_puma = InlineKeyboardMarkup(inline_keyboard=btn_puma)
+
+
+
+
+btn_vans = [
+    [
+        InlineKeyboardButton(text="Вперед", callback_data="next_vans"), # кнопки которые переключают модель кед
+        InlineKeyboardButton(text="Назад", callback_data="back_vans")
+    ],
+    [
+        InlineKeyboardButton(text="Вернуться", callback_data="catalog_pressed")
+    ]
+] 
+kb_vans = InlineKeyboardMarkup(inline_keyboard=btn_vans)
+
+
+
+btn_fila = [
+    [
+        InlineKeyboardButton(text="Вперед", callback_data="next_fila"), # кнопки которые переключают модель кед
+        InlineKeyboardButton(text="Назад", callback_data="back_fila")
+    ],
+    [
+        InlineKeyboardButton(text="Вернуться", callback_data="catalog_pressed")
+    ]
+] 
+kb_fila = InlineKeyboardMarkup(inline_keyboard=btn_fila)
